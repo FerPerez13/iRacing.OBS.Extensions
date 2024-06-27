aacc45b3cdcac2ffe944560cb7481c6bc6501ab6
FerPerez13 <fer13vlc@gmail.com>
2024-06-27 22:46:09 +0200

    Fix Problem with Resources images

--- obk.iRacing.OBS.Extensions.App/Helpers/HtmlStrings.cs
+++ obk.iRacing.OBS.Extensions.App/Helpers/HtmlStrings.cs
@@ -7,7 +7,7 @@ public static class HtmlStrings
     public static string LicenseHtml(string type, string license, string irating, string sr)
     {
         return
-            $"<head><meta http-equiv=\"refresh\" content=\"1\"></head><html><table><tr><td><img src=\"{PathHelper.ResourcesPath}/{type.ToLower()}_{license.ToLower()}.svg\"></td><td><div style=\"font-size:125px;color:#fff;font-weight:700;font-family:verdana\">{sr}</div><div style=\"font-size:125px;color:#fff;font-weight:700;font-family:verdana\">{irating}</div></td></tr></table></html>";
+            $"<head><meta http-equiv=\"refresh\" content=\"1\"></head><html><table><tr><td><img src=\"..\\resources\\{type.ToLower()}_{license.ToLower()}.svg\"></td><td><div style=\"font-size:125px;color:#fff;font-weight:700;font-family:verdana\">{sr}</div><div style=\"font-size:125px;color:#fff;font-weight:700;font-family:verdana\">{irating}</div></td></tr></table></html>";
     }
     
     public static string RecentEventHtml(string eventName, string carName, string trackName, int startingPosition, int finishPosition)
